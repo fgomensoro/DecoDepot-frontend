febcc03bbc3e388cb68f22bd31380cf98a580caa
German-uruguay <106714484+German-uruguay@users.noreply.github.com>
2022-11-09 12:05:44 -0300

    inicios

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,7 @@
+import styles from "./Footer.module.css";
+
+function Footer() {
+  return <h1 className={styles.title}>Footer</h1>;
+}
+
+export default Footer;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,7 @@
+import styles from "./Navbar.module.css";
+
+function Navbar() {
+  return <h1 className={styles.title}>Navbar</h1>;
+}
+
+export default Navbar;
