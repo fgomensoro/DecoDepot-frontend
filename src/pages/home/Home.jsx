bfa7673deda02ac137c476abc701dd107875e63c
TomasZerbino <103967982+TomasZerbino@users.noreply.github.com>
2022-11-10 14:36:30 -0300

    se agrega destacados

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Featured from "../../components/Featured/Featured";
 import Hero from "../../components/hero/Hero";
 
 function Home() {
   return (
     <>
       <Hero />
+      <Featured />
     </>
   );
 }
