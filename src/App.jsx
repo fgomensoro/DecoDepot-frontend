127a6646380c7d5c22de47e925819096bce198dd
German-uruguay <106714484+German-uruguay@users.noreply.github.com>
2022-11-10 07:08:57 -0300

    gfeat navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.module.css";
-import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <div className="">
       <Navbar />
-      <Footer />
     </div>
   );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,71 @@
 import styles from "./Navbar.module.css";
+import { Link } from "react-router-dom";
 
 function Navbar() {
-  return <h1 className={styles.title}>Navbar</h1>;
+  return (
+    <nav class="navbar navbar-expand-lg bg-light">
+      <div class="container-fluid d-flex  justify-content-around">
+        <ul class="navbar-nav  mb-2 mb-lg-0">
+          <li class="nav-item">
+            <Link to="/" className={`${styles.customNavLink} nav-link`}>
+              Shop
+            </Link>
+          </li>
+          <li class="nav-item">
+            <Link to="/" className={`${styles.customNavLink} nav-link`}>
+              About
+            </Link>
+          </li>
+
+          <li class="nav-item">
+            <Link to="/" className={`${styles.customNavLink} nav-link`}>
+              Our mission
+            </Link>
+          </li>
+        </ul>
+        <ul class="navbar-nav  mb-2 mb-lg-0">
+          <li class="nav-item">
+            <Link to="/" className={`${styles.customNavLink} nav-link`}>
+              Shop
+            </Link>
+          </li>
+        </ul>
+        <ul class="navbar-nav  mb-2 mb-lg-0">
+          <li class="nav-item">
+            <Link to="/" className={`${styles.customNavLink} nav-link`}>
+              Cart
+            </Link>
+          </li>
+
+          <li class="nav-item">
+            <Link to="/" className={`${styles.customNavLink} nav-link`}>
+              My accout
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
+
+/* 
+<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+<li class="nav-item">
+  <Link to="/" className="nav-link">
+    Shop
+  </Link>
+</li>
+<li class="nav-item">
+  <Link to="/" className="nav-link">
+    About
+  </Link>
+</li>
+
+<li class="nav-item">
+  <Link to="/" className="nav-link">
+    Our mission
+  </Link>
+</li>
+</ul> */
