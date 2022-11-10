158f2074007a29d52db6fa4f000fb3d9104b256e
fgomensoro <102269369+fgomensoro@users.noreply.github.com>
2022-11-10 16:09:12 -0300

    feat: combo

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.module.css";
 import Navbar from "./components/navbar/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
+import Combo from "./components/combo/Combo";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/combo" element={<Combo />} />
         </Routes>
       </div>
     </div>
