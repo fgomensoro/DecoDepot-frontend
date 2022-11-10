2a20783e3ebd550a7d0fda5e03166e17b5a67bc1
Joel Dominguez <joeru.desu@gmail.com>
2022-11-10 14:34:08 -0300

    feat: Reviews

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -13,7 +13,7 @@ function Hero() {
     >
       <img className={styles.image} src={url} alt="" />
 
-      <div className={`${styles.textContainer2} col-12 col-md-6`}>
+      <div className={`${styles.textContainer} col-12 col-md-6`}>
         <h1 className={styles.title}>Design you home interior</h1>
         <button className={styles.button}>Shop Now</button>
       </div>
--- src/components/reviews/Reviews.jsx
+++ src/components/reviews/Reviews.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import styles from "./Reviews.module.css";
 
 function Reviews() {
   const [reviews, setReviews] = useState(null);
@@ -20,36 +21,46 @@ function Reviews() {
 
   return (
     reviews && (
-      <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
-        <div className="carousel-inner">
-          <div className="carousel-item active" data-bs-interval="10000">
-            <img src="..." className="d-block w-100" alt="..." />
+      <div className={styles.section}>
+        <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-inner">
+            {reviews.map((review, index) => {
+              if (index === 0) {
+                return (
+                  <div class="carousel-item active" data-bs-interval="5000">
+                    <h2>"{review.comment}"</h2>
+                    <p>{review.author}</p>
+                  </div>
+                );
+              } else {
+                return (
+                  <div class="carousel-item" data-bs-interval="5000">
+                    <h2 className={styles.comment}>"{review.comment}"</h2>
+                    <p className={styles.author}>{review.author}</p>
+                  </div>
+                );
+              }
+            })}
           </div>
-          <div className="carousel-item" data-bs-interval="2000">
-            <img src="..." className="d-block w-100" alt="..." />
-          </div>
-          <div className="carousel-item">
-            <img src="..." className="d-block w-100" alt="..." />
-          </div>
+          <button
+            class="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleInterval"
+            data-bs-slide="prev"
+          >
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button
+            class="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleInterval"
+            data-bs-slide="next"
+          >
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
         </div>
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleInterval"
-          data-bs-slide="prev"
-        >
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleInterval"
-          data-bs-slide="next"
-        >
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Next</span>
-        </button>
       </div>
     )
   );
