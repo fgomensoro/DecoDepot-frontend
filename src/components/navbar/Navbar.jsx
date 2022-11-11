1ca44bbd567b70d1ee80f1d62e9e1bf98e4b3ec9
German-uruguay <106714484+German-uruguay@users.noreply.github.com>
2022-11-10 21:20:45 -0300

    navbar change collor on scroll

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,24 @@
 import styles from "./Navbar.module.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function Navbar() {
   const logoUrl = "decoDepotLogo.png";
+  const [colorChange, setColorchange] = useState(false);
+  const changeNavbarColor = () => {
+    if (window.scrollY >= 80) {
+      setColorchange(true);
+    } else {
+      setColorchange(false);
+    }
+  };
+  window.addEventListener("scroll", changeNavbarColor);
   return (
-    <nav className={`${styles.backGrey} navbar navbar-expand-lg `}>
+    <nav
+      className={`${
+        colorChange ? styles.backBlue : styles.backGrey
+      } navbar navbar-expand-lg fixed-top`}
+    >
       <div className="container d-flex  justify-content-between">
         <ul className="navbar-nav  mb-2 mb-lg-0">
           <li className="nav-item">
