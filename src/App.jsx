42a45390841a4beed34346201d2a71b43fbbacc3
Joel Dominguez <joeru.desu@gmail.com>
2022-11-13 14:07:47 -0300

    fix: admin tables switch to new routes each

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import "./App.module.css";
 import { Routes, Route } from "react-router-dom";
 import styles from "./App.module.css";
 import Home from "./pages/home/Home";
-import Navbar from "./components/navbar/Navbar";
-import Footer from "./components/footer/Footer";
 import SignUp from "./pages/signUp/SignUp";
 import Login from "./pages/login/Login";
 import Products from "./pages/products/Products";
 import Admin from "./pages/admin/Admin";
 import Checkout from "./pages/checkout/Checkout";
+import ProductsTable from "./components/tables/ProductsTable";
+import OrdersTable from "./components/tables/OrdersTable";
+import PacksTable from "./components/tables/PacksTable";
+import ReviewsTable from "./components/tables/ReviewsTable";
+import UsersTable from "./components/tables/UsersTable";
 
 function App() {
   return (
@@ -18,8 +21,13 @@ function App() {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
         <Route path="/store" element={<Products />} />
-        <Route path="/admin" element={<Admin />} />
         <Route path="/checkout" element={<Checkout />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/admin/products" element={<ProductsTable />} />
+        <Route path="/admin/orders" element={<OrdersTable />} />
+        <Route path="/admin/packs" element={<PacksTable />} />
+        <Route path="/admin/reviews" element={<ReviewsTable />} />
+        <Route path="/admin/users" element={<UsersTable />} />
       </Routes>
     </div>
   );
--- src/components/tables/UsersTable.jsx
+++ src/components/tables/UsersTable.jsx
@@ -1,5 +1,9 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import styles from "./AdminLists.module.css";
+import AdminNav from "../adminNav/AdminNav";
+import Navbar from "../navbar/Navbar";
+import Footer from "../footer/Footer";
 
 function UsersTable() {
   const [users, setUsers] = useState(null);
@@ -13,50 +17,56 @@ function UsersTable() {
         //   Authorization: "Bearer " + token,
         // },
       });
-      console.log(response.data.users);
       setUsers(response.data.users);
     };
     getUsers();
   }, []); // eslint-disable-line
 
   return (
-    users && (
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Firstname</th>
-            <th scope="col">Lastname</th>
-            <th scope="col">Email</th>
-            <th scope="col">Address</th>
-            <th scope="col">Phone</th>
-            <th scope="col">Admin</th>
-            <th scope="col">Orders</th>
-            <th scope="col">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user, index) => {
-            return (
-              <tr key={user._id}>
-                <th scope="row">{index}</th>
-                <td>{user.firstname}</td>
-                <td>{user.lastname}</td>
-                <td>{user.email}</td>
-                <td>{user.address}</td>
-                <td>{user.phoneNumber}</td>
-                <td>{user.admin}</td>
-                <td>{user.orders}</td>
-                <td>
-                  <button>Edit</button>
-                  <button>Delete</button>
-                </td>
+    <div>
+      <Navbar />
+      <div className={`${styles.body} container`}>
+        <AdminNav active={"Users"} />
+        {users && (
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Firstname</th>
+                <th scope="col">Lastname</th>
+                <th scope="col">Email</th>
+                <th scope="col">Address</th>
+                <th scope="col">Phone</th>
+                <th scope="col">Admin</th>
+                <th scope="col">Orders</th>
+                <th scope="col">Actions</th>
               </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    )
+            </thead>
+            <tbody>
+              {users.map((user, index) => {
+                return (
+                  <tr key={user._id}>
+                    <th scope="row">{index}</th>
+                    <td>{user.firstname}</td>
+                    <td>{user.lastname}</td>
+                    <td>{user.email}</td>
+                    <td>{user.address}</td>
+                    <td>{user.phoneNumber}</td>
+                    <td>{user.admin}</td>
+                    <td>{user.orders}</td>
+                    <td>
+                      <button>Edit</button>
+                      <button>Delete</button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        )}
+      </div>
+      <Footer />
+    </div>
   );
 }
 
