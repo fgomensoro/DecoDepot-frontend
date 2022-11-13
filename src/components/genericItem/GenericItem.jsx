c5da3deeb69996df597bdf290f4070f2ef207bb4
TomasZerbino <103967982+TomasZerbino@users.noreply.github.com>
2022-11-13 18:19:41 -0300

    style: animated buttons

--- src/components/genericItem/GenericItem.jsx
+++ src/components/genericItem/GenericItem.jsx
@@ -11,36 +11,42 @@ function GenericItem({ product }) {
   return (
     <div className={`${styles.smallCard} mt-3`}>
       <div className={`${styles.cardCombo1}`}>
-        <Link className="text-decoration-none" to={`/detail/${product._id}`}>
-          <div
-            className={styles.longImage}
-            style={{
-              backgroundImage: `url(${process.env.REACT_APP_IMAGE_PATH + product.images[2]})`,
-            }}
-          ></div>
-          {/*         <img
-          className={styles.smallImage}
-          src={`${process.env.REACT_APP_IMAGE_PATH + product.images[2]}`}
-          alt="comboImage"
-        /> */}
-          <div className={styles.inline}>
-            <h4 className={styles.titleH4}>{product.name}</h4>
-            <p className={styles.titleH4}>${product.price}</p>
+        <div
+          className={styles.imgContainer}
+          // style={{
+          //   backgroundImage: `url(${process.env.REACT_APP_IMAGE_PATH + product.images[2]})`,
+          // }}
+        >
+          <div className={styles.detailsBtns}>
+            <Link className={`${styles.btnSee} text-decoration-none`} to={`/detail/${product._id}`}>
+              See More
+            </Link>
+            <button className={styles.btnAdd}>Add To Cart</button>
           </div>
-          <div className={styles.inline}>
-            {description.length > 65 && (
-              <p className={styles.paragraphCard}>{description.slice(0, 65)} ...</p>
-            )}
-            {description.length < 66 && <p className={styles.paragraphCard}>{description}</p>}
-            <p className={`${styles.paragraphCard2} ${styles.linethrough}`}>
-              ${Math.round(product.price * 1.25)}
-            </p>
-          </div>
-          <div className={styles.smallCard}>
-            <StarRatings rating={rating} starDimension="1rem" starSpacing="0px" />
-            {`  ${numberReviews}`} Reviews
-          </div>
-        </Link>
+          <img
+            className={styles.img}
+            src={`${process.env.REACT_APP_IMAGE_PATH + product.images[2]}`}
+            alt="comboImage"
+          />
+        </div>
+
+        <div className={styles.inline}>
+          <h4 className={styles.titleH4}>{product.name}</h4>
+          <p className={styles.titleH4}>${product.price}</p>
+        </div>
+        <div className={styles.inline}>
+          {description.length > 65 && (
+            <p className={styles.paragraphCard}>{description.slice(0, 65)} ...</p>
+          )}
+          {description.length < 66 && <p className={styles.paragraphCard}>{description}</p>}
+          <p className={`${styles.paragraphCard2} ${styles.linethrough}`}>
+            ${Math.round(product.price * 1.25)}
+          </p>
+        </div>
+        <div className={styles.smallCard}>
+          <StarRatings rating={rating} starDimension="1rem" starSpacing="0px" />
+          {`  ${numberReviews}`} Reviews
+        </div>
       </div>
     </div>
   );
