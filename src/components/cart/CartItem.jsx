21759f90785a3e97bc9d43fb220186a185884f0d
German-uruguay <106714484+German-uruguay@users.noreply.github.com>
2022-11-15 20:36:35 -0300

    cart fix

--- src/components/cart/CartItem.jsx
+++ src/components/cart/CartItem.jsx
@@ -1,23 +1,20 @@
 import styles from "./CartItem.module.css";
 
-import { useState, useRef } from "react";
+import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addItem, removeItem } from "../../redux/cartSlice";
-import { useSelector } from "react-redux";
-import { current } from "@reduxjs/toolkit";
 
 function CartItem({ product }) {
-  const [quantity, setQuantity] = useState(0);
+  const [quantity, setQuantity] = useState(product.qty);
   const dispatch = useDispatch();
-  const cart = useSelector((state) => state.cart.items);
-  const formRef = useRef();
-  const handleMinus = () => {
-    if (quantity > 0) {
-      setQuantity(quantity - 1);
-    }
+
+  const handleMinus = (e) => {
+    dispatch(removeItem({ name: product.name, qty: 1 }));
+    setQuantity(quantity - 1);
   };
 
-  const handlePlus = () => {
+  const handlePlus = (e) => {
+    dispatch(addItem({ name: product.name, qty: 1 }));
     setQuantity(quantity + 1);
   };
   function handleSubmitAdd(event) {
@@ -36,10 +33,6 @@ function CartItem({ product }) {
     );
   }
 
-  function handleSubmitRemove(event) {
-    event.preventDefault();
-    dispatch(removeItem({ name: product.name, qty: quantity })), setQuantity(0);
-  }
   function handleSubmitClear(event) {
     event.preventDefault();
     dispatch(removeItem({ name: product.name, qty: 10000 }));
@@ -60,50 +53,27 @@ function CartItem({ product }) {
                 <h3 className="text-center">{"$" + product.price}</h3>
               </div>
             </div>
-            <div className="col-7 d-flex align-items-center">
+            <div className="col-7 d-flex align-items-center ">
               <div>
                 <h2>{product.name}</h2>
                 <span>Ships in 7-10 Days</span>
-              </div>
-            </div>
-            <div className={`${styles.formContainer} d-flex align-items-center`}>
-              <form ref={formRef} className={styles.form} /* onSubmit={handleSubmitAdd} */>
-                <div className={styles.inline}>
-                  <button
-                    className={`${styles.plus} btn`}
-                    type="button"
-                    onClick={() => handleMinus()}
-                  >
+                <div className={styles.input}>
+                  <button className={`${styles.plus} btn`} type="button" onClick={handleMinus}>
                     -
                   </button>
-                  <input
-                    type="text"
-                    className={styles.quantity}
-                    value={quantity}
-                    name="quantity"
-                    onChange={(event) => setQuantity(event.target.value)}
-                  />
-                  <button
-                    className={`${styles.minus} btn`}
-                    type="button"
-                    onClick={() => handlePlus()}
-                  >
+                  <input type="text" className={styles.quantity} value={quantity} name="quantity" />
+                  <button className={`${styles.minus} btn`} type="button" onClick={handlePlus}>
                     +
-                  </button>{" "}
-                  <button type="submit" className={styles.shopBtn} onClick={handleSubmitAdd}>
-                    Add item
                   </button>
-                  <button type="submit" className={styles.shopBtn} onClick={handleSubmitRemove}>
-                    Remove
-                  </button>
-                </div>{" "}
-              </form>{" "}
-            </div>{" "}
+                </div>
+              </div>
+            </div>
+
             <div className="d-flex justify-content-between mt-4">
               <p className={styles.amount}>You have {product.qty} of this in your cart</p>
               <form onSubmit={handleSubmitClear}>
                 <button type="submit" className={styles.delete}>
-                  <i className={`${styles.trash} bi bi-trash`}></i>{" "}
+                  <i className={`${styles.trash} bi bi-trash`}></i>
                 </button>
               </form>
             </div>
