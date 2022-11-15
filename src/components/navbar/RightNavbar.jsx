90884e416cb443f22aadfe3f8cb4e9dd718f0c8a
Joel Dominguez <joeru.desu@gmail.com>
2022-11-15 16:46:47 -0300

    fix separating navbar components

---
+++ src/components/navbar/RightNavbar.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import styles from "./Navbar.module.css";
+import { Link } from "react-router-dom";
+import Cart from "../cart/Cart";
+
+function RightNavbar({ showCart, handleCloseCart, cart }) {
+  return (
+    <Offcanvas
+      show={showCart}
+      onHide={handleCloseCart}
+      placement={"end"}
+      className={styles.cartSideBar}
+    >
+      <Offcanvas.Header className={`${styles.cartHeader} `}>
+        <button onClick={handleCloseCart} className={styles.btnClose}>
+          <i className="bi bi-chevron-left"></i>
+        </button>
+        <h5
+          className={`${
+            cart.items.length !== 0 ? styles.cartTitle : styles.cartTitle2
+          } text-center`}
+        >
+          My cart
+        </h5>
+      </Offcanvas.Header>
+
+      <Offcanvas.Body className={`${cart.items.length !== 0 ? styles.cartBody1 : styles.cartBody}`}>
+        <div className={styles.rssBlock}>
+          <span className={styles.marqueeStyle}>&nbsp;FREE SHIPPING ON ORDERS OVER $32</span>
+          {/* <span className={styles.marqueeStyle2}>
+          &nbsp; Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+        </span> */}
+        </div>
+        <Cart />
+      </Offcanvas.Body>
+      {cart.items.length !== 0 && (
+        <div
+          className={`${styles.cartFooter} d-flex p-3 align-items-center justify-content-center order-2 w-100`}
+        >
+          <div className="d-flex flex-column align-items-center">
+            <span>SUBTOTAL</span>
+            <h3>{cart.total}$</h3>
+          </div>
+
+          <Link to="/checkout" className={styles.linkContinue}>
+            <button className={styles.btnContinue}> Continue to checkout</button>
+          </Link>
+        </div>
+      )}
+    </Offcanvas>
+  );
+}
+
+export default RightNavbar;
