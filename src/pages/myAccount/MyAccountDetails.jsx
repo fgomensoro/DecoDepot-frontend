b9e07b66ed0bc179e18da0327c4c4308e58b09d7
fgomensoro <102269369+fgomensoro@users.noreply.github.com>
2022-11-18 15:43:23 -0300

    Feat: user detailes myacount

--- src/pages/myAccount/MyAccountDetails.jsx
+++ src/pages/myAccount/MyAccountDetails.jsx
@@ -1,7 +1,12 @@
 import styles from "./MyAccount.module.css";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function MyAccount() {
+  const user = useSelector((state) => state.user);
+
+  console.log(user);
+
   return (
     <div className="row">
       <div className="col-4">
@@ -23,12 +28,37 @@ function MyAccount() {
             <Link className={styles.link} onClick={() => handleLogout()} to="/">
               Log Out
             </Link>
-           
           </div>
           <div className="col-2"></div>
         </div>
       </div>
-      <div className="col-8"></div>
+      <div className="col-8">
+        <div className={styles.container2}>
+          <div>
+            <small className={styles.title}>Name: </small>
+            <small className={styles.detail}>{user.firstname}</small>
+          </div>
+          <div>
+            <h3 className={styles.title}>Lastname: </h3>
+            <small className={styles.detail}>{user.lastname}</small>
+          </div>
+          <div>
+            <small className={styles.title}>Email: </small>
+            <small className={styles.detail}>{user.email}</small>
+          </div>
+          <div>
+            <small className={styles.title}>Address: </small>
+            <small className={styles.detail}>{user.address}</small>
+          </div>
+          <div>
+            <small className={styles.title}>Phone Number: </small>
+            <small className={styles.detail}></small>
+          </div>
+          <div className="d-flex">
+            <Link className={`${styles.edit} btn btn-light`}>Edit</Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
