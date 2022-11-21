aaf9702e54274b1dfb49aea85ed24da8a0365995
fgomensoro <102269369+fgomensoro@users.noreply.github.com>
2022-11-21 15:37:04 -0300

    fix: responsive in myaccount

--- src/components/myAccount/Product.jsx
+++ src/components/myAccount/Product.jsx
@@ -5,12 +5,12 @@ function Product({ product, order }) {
 
   return (
     <div className="card-body p-4">
-      <div className="d-flex flex-row pb-2">
-        <div className="flex-fill">
+      <div className="d-flex pb-2">
+        <div>
           <h5 className="bold">{product.name}</h5>
           <p className="text-muted"> Qty: {product.qty} items</p>
           <h4 className="mb-3">${dollarUSLocale.format(product.price)}</h4>
-          <p className="text-muted">
+          <p className="text-muted ps-0 px-2">
             Status: <span className="text-body">{order.status}</span>
           </p>
         </div>
