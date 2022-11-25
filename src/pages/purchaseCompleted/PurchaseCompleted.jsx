fa2ce1d5d322f7ff6aa6f681ff922993b1e1f429
fgomensoro <102269369+fgomensoro@users.noreply.github.com>
2022-11-25 09:28:23 -0300

    fix: responsive purchase view

--- src/pages/purchaseCompleted/PurchaseCompleted.jsx
+++ src/pages/purchaseCompleted/PurchaseCompleted.jsx
@@ -27,7 +27,7 @@ function PurchaseCompleted() {
   return (
     order && (
       <div className={styles.container}>
-        <h1>Thanks For Your Purchase!</h1>
+        <h1 className={styles.title}>Thanks For Your Purchase!</h1>
         <div className="d-flex justify-content-center">
           <Order order={order} />
         </div>
