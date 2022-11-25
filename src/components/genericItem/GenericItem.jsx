256d89a1b2ec4bf1a7c6efb4aa5e8180fae4c13f
Joel Dominguez <joeru.desu@gmail.com>
2022-11-25 11:16:09 -0300

    fix cleaning

--- src/components/genericItem/GenericItem.jsx
+++ src/components/genericItem/GenericItem.jsx
@@ -14,39 +14,34 @@ function GenericItem({ product }) {
 
   return (
     <div className={`${styles.smallCard} mt-3`}>
-      <div className={`${styles.cardCombo1}`}>
-        <div className={styles.imgContainer}>
-          <div className={styles.detailsBtns}>
-            <Link
-              className={`${styles.btnSee} text-decoration-none`}
-              to={`/detail/${product.slug}`}
-            >
-              See more
-            </Link>
-            <AddToCartButton product={product} btnClassName={"btnAdd"} toggle={true} />
-          </div>
-          <img className={styles.img} src={urlImg} alt="Product Image" />
+      <div className={styles.imgContainer}>
+        <div className={styles.detailsBtns}>
+          <Link className={`${styles.btnSee} text-decoration-none`} to={`/detail/${product.slug}`}>
+            See more
+          </Link>
+          <AddToCartButton product={product} btnClassName={"btnAdd"} toggle={true} />
         </div>
+        <img className={styles.img} src={urlImg} alt="Product Image" />
+      </div>
 
-        <div className={styles.inline}>
-          <h4 className={styles.titleH4}>{product.name}</h4>
-          <div>
-            <p className={styles.titleH4}>${product.price}</p>
-            <p className={`${styles.paragraphCard2} ${styles.linethrough}`}>
-              ${Math.round(product.price * 1.25)}
-            </p>
-          </div>
+      <div className={styles.inline}>
+        <h4 className={styles.titleH4}>{product.name}</h4>
+        <div>
+          <p className={styles.titleH4}>${product.price}</p>
+          <p className={`${styles.paragraphCard2} ${styles.linethrough}`}>
+            ${Math.round(product.price * 1.25)}
+          </p>
         </div>
-        <div className={styles.inline}>
-          {description.length > 65 && (
-            <p className={styles.paragraphCard}>{description.slice(0, 65)} ...</p>
-          )}
-          {description.length < 66 && <p className={styles.paragraphCard}>{description}</p>}
-        </div>
-        <div className={styles.smallCard}>
-          <StarRatings rating={rating} starDimension="1rem" starSpacing="0px" />
-          {`  ${numberReviews}`} Reviews
-        </div>
+      </div>
+      <div className={styles.inline}>
+        {description.length > 65 && (
+          <p className={styles.paragraphCard}>{description.slice(0, 65)} ...</p>
+        )}
+        {description.length < 66 && <p className={styles.paragraphCard}>{description}</p>}
+      </div>
+      <div className={styles.smallCard}>
+        <StarRatings rating={rating} starDimension="1rem" starSpacing="0px" />
+        {`  ${numberReviews}`} Reviews
       </div>
     </div>
   );
