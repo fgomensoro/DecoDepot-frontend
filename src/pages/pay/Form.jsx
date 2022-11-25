ed8deaed0427941a4118500406829c0f85863e38
Joel Dominguez <joeru.desu@gmail.com>
2022-11-25 13:00:08 -0300

    fix cleaning Pay.jsx and Pay.module.jsx

--- src/pages/pay/Form.jsx
+++ src/pages/pay/Form.jsx
@@ -1,20 +1,12 @@
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import axios from "axios";
-import styles from "./Pay.module.css";
+import styles from "./Form.module.css";
 import { BsCheckCircleFill } from "react-icons/bs";
 import { FaCcMastercard, FaCcVisa, FaCcAmex, FaCcPaypal, FaLock } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { clearCart } from "../../redux/cartSlice";
-
-// const normalizeCardNumber = (value) => {
-//   value
-//     .replace(/\s/g, "")
-//     .match(/.{1,4}/g)
-//     .join(" ")
-//     .substr(0, 19) || "";
-// };
 
 function CheckoutForm() {
   const state = useSelector((state) => state);
@@ -61,10 +53,10 @@ function CheckoutForm() {
   };
 
   return (
-    <div>
-      <h2 className={`${styles.shippingAddress} mb-4`}>Shipping</h2>
+    <div className={styles.formWrapper}>
+      <h2 className={`${styles.shippingAddress} mb-4 mx-auto`}>Shipping</h2>
       <form className={styles.form2}>
-        <div className={`${styles.group2} form-group`}>
+        <div className={`d-flex form-group`}>
           <button disabled className={`${styles.button} btn`}>
             <BsCheckCircleFill className={`${styles.check}`} />
           </button>
@@ -73,94 +65,95 @@ function CheckoutForm() {
       </form>
 
       <p className={styles.line}></p>
+      <div className={styles.formWrapper}>
+        <h2 className={`${styles.shippingAddress} mb-4`}>Billing & payment</h2>
+        <form className={styles.form3}>
+          <div className={`d-flex justify-content-between form-group`}>
+            <div className="d-flex">
+              <button disabled className={`${styles.button2} btn`}>
+                <BsCheckCircleFill className={`${styles.check}`} />
+              </button>
+              <h4 className={`${styles.item3} ${styles.freeShipping}`}>Credit Card</h4>
+            </div>
+            <div className="mx-1">
+              <FaCcMastercard className={styles.cardsIcons} />
 
-      <h2 className={`${styles.shippingAddress} mb-4`}>Billing & payment</h2>
-      <form className={styles.form3}>
-        <div className={`${styles.group} form-group`}>
-          <div className="d-flex">
-            <button disabled className={`${styles.button2} btn`}>
-              <BsCheckCircleFill className={`${styles.check}`} />
+              <FaCcVisa className={styles.cardsIcons} />
+
+              <FaCcAmex className={styles.cardsIcons} />
+
+              <FaCcPaypal className={styles.cardsIcons} />
+            </div>
+          </div>
+        </form>
+
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <div className={`d-flex mb-4`}>
+            <label className={styles.label} htmlFor="nameOnCard">
+              Name on card
+            </label>
+            <input
+              type="text"
+              className={`${styles.item} form-control`}
+              placeholder="Name on card"
+              value={fullName}
+              onChange={(e) => setFullName(e.target.value)}
+            />
+          </div>
+          {error && !fullName && <span className={styles.message}>Required field</span>}
+
+          <div className={`d-flex mb-4 position-relative`}>
+            <label className={styles.label} htmlFor="cardNumber">
+              Card number
+            </label>
+            <input
+              type="text"
+              className={`${styles.item} form-control`}
+              id="cardNumber"
+              placeholder="0000 0000 0000 0000"
+              value={cardNumber}
+              autoComplete="cc-number"
+              onChange={(e) => setCardNumber(e.target.value)}
+            />
+            <FaLock className={styles.faLock} />
+          </div>
+          {error && !cardNumber && <span className={styles.message}>Required field</span>}
+
+          <div className={`d-flex mb-4`}>
+            <label className={styles.label} htmlFor="expiration">
+              Expiration
+            </label>
+            <input
+              type="text"
+              className={`${styles.item} form-control`}
+              placeholder="MM / AA"
+              value={expirationDate}
+              onChange={(e) => setExpirationDate(e.target.value)}
+            />
+          </div>
+          {error && !expirationDate && <span className={styles.message}>Required field</span>}
+          <div className={`d-flex mb-4`}>
+            <label className={styles.label} htmlFor="cvv">
+              CVV
+            </label>
+            <input
+              type="number"
+              className={`${styles.item} form-control`}
+              placeholder="CVV"
+              value={securityCode}
+              onChange={(e) => setSecurityCode(e.target.value)}
+            />
+          </div>
+          {error && !securityCode && <span className={styles.message}>Required field</span>}
+
+          <p className={styles.message}>{backendMessage}</p>
+          <div className={`${styles.links} `}>
+            <button type="submit" className={`${styles.placeMyOrder} btn btn-block`}>
+              Place my order
             </button>
-            <h4 className={`${styles.item3} ${styles.freeShipping}`}>Credit Card</h4>
           </div>
-          <div className="mx-1">
-            <FaCcMastercard className={styles.cardsIcons} />
-
-            <FaCcVisa className={styles.cardsIcons} />
-
-            <FaCcAmex className={styles.cardsIcons} />
-
-            <FaCcPaypal className={styles.cardsIcons} />
-          </div>
-        </div>
-      </form>
-
-      <form onSubmit={handleSubmit} className={styles.form}>
-        <div className={`d-flex mb-4`}>
-          <label className={styles.label} htmlFor="nameOnCard">
-            Name on card
-          </label>
-          <input
-            type="text"
-            className={`${styles.item} form-control`}
-            placeholder="Name on card"
-            value={fullName}
-            onChange={(e) => setFullName(e.target.value)}
-          />
-        </div>
-        {error && !fullName && <span className={styles.message}>Required field</span>}
-
-        <div className={`d-flex mb-4 position-relative`}>
-          <label className={styles.label} htmlFor="cardNumber">
-            Card number
-          </label>
-          <input
-            type="text"
-            className={`${styles.item} form-control`}
-            id="cardNumber"
-            placeholder="0000 0000 0000 0000"
-            value={cardNumber}
-            autoComplete="cc-number"
-            onChange={(e) => setCardNumber(e.target.value)}
-          />
-          <FaLock className={styles.faLock} />
-        </div>
-        {error && !cardNumber && <span className={styles.message}>Required field</span>}
-
-        <div className={`d-flex mb-4`}>
-          <label className={styles.label} htmlFor="expiration">
-            Expiration
-          </label>
-          <input
-            type="text"
-            className={`${styles.item} form-control`}
-            placeholder="MM / AA"
-            value={expirationDate}
-            onChange={(e) => setExpirationDate(e.target.value)}
-          />
-        </div>
-        {error && !expirationDate && <span className={styles.message}>Required field</span>}
-        <div className={`d-flex mb-4`}>
-          <label className={styles.label} htmlFor="cvv">
-            CVV
-          </label>
-          <input
-            type="number"
-            className={`${styles.item} form-control`}
-            placeholder="CVV"
-            value={securityCode}
-            onChange={(e) => setSecurityCode(e.target.value)}
-          />
-        </div>
-        {error && !securityCode && <span className={styles.message}>Required field</span>}
-
-        <p className={styles.message}>{backendMessage}</p>
-        <div className={`${styles.links} `}>
-          <button type="submit" className={`${styles.placeMyOrder} btn btn-block`}>
-            Place my order
-          </button>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   );
 }
